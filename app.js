0669050e635f15ae68c500c034cf4408207c212f
Ivan Knezevic <ivan.knezevic@wackes.com>
2023-08-28 18:30:49 +0200

    CORS

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.get("/deezer/search/", async (req, res) => {
