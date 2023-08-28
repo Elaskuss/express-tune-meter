0b23f019efd04f4b5f8e25c753ad6f319b80c5fb
Ivan Knezevic <ivan.knezevic@wackes.com>
2023-08-28 18:05:48 +0200

    server add deeezer

--- app.js
+++ app.js
@@ -4,7 +4,45 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  if (req.method !== "GET") {
+      res.status(405).json({ error: "Method not allowed" });
+  } else {
+      next();
+  }
+});
+
+app.get("/deezer/search/", async (req, res) => {
+  const artist = req.query.artist;
+  const track = req.query.track;
+
+  if (!artist || !track) {
+      return res
+          .status(400)
+          .json({ error: "Missing artist or track query parameters" });
+  }
+
+  const deezerApiUrl = `https://api.deezer.com/search?q=artist:"${artist}"track:"${track}"`;
+
+  try {
+      const response = await fetch(deezerApiUrl);
+      const data = await response.json();
+      res.json(data);
+  } catch (error) {
+      res.status(500).json({
+          error: "An error occurred while fetching data from Deezer.",
+      });
+  }
+});
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
